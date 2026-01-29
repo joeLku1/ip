690e9e1193afe422493876907ed3d6712d23d4b1
joeLku1 <joelku77@gmail.com>
2026-01-29 15:21:56 +0800

    Level 1: Echo
    Added user name for customized greeting, added echo for user commands, added exit condition when user types the bye command.

--- src/main/java/clowns.java
+++ src/main/java/clowns.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class clowns {
     public static void main(String[] args) {
@@ -14,10 +15,31 @@ public class clowns {
         """;
 
         System.out.println(logo);
-        System.out.println("Hello, I am clowning!");
-        System.out.println("---------------------------------\n");
-        System.out.println("Clowning complete.");
-        System.out.println("Goodbye!");
-        System.out.println("---------------------------------\n");
+        System.out.println("\tHello, I am clowning!");
+        System.out.println("\t---------------------------------\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\tEnter your name:\n"); 
+        String name = scanner.nextLine();
+        System.out.println("\nWelcome, " + name + "! Let's clown together! Enter your command below:\n");
+
+        boolean exitFlag = true;
+
+        while (exitFlag) { 
+            String usr_input = scanner.nextLine();
+            if (usr_input.equalsIgnoreCase("exit")) {
+                exitFlag = false;
+            } else {
+                System.out.println("\t---------------------------------\n");
+                System.out.println(usr_input);
+                System.out.println("\t---------------------------------\n");
+            }
+        }
+
+        System.out.println("\tClowning complete.");
+        System.out.println("\tGoodbye fellow clown!");
+        System.out.println("\t---------------------------------\n");
+
+        scanner.close();
     }
 }

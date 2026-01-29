5056a4572973471d81b7c557b4d24d07b6bf7c14
joeLku1 <joelku77@gmail.com>
2026-01-29 15:44:21 +0800

    Level 2. Add, List
    Every command is now stored in a list, list is displayed on the 'list' command

--- src/main/java/clowns.java
+++ src/main/java/clowns.java
@@ -15,30 +15,44 @@ public class clowns {
         """;
 
         System.out.println(logo);
-        System.out.println("\tHello, I am clowning!");
-        System.out.println("\t---------------------------------\n");
+        System.out.println("  Hello, I am clowning!");
+        System.out.println("  ---------------------------------\n");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("\tEnter your name:\n"); 
+        System.out.print("  Enter your name:\n"); 
         String name = scanner.nextLine();
         System.out.println("\nWelcome, " + name + "! Let's clown together! Enter your command below:\n");
 
         boolean exitFlag = true;
+        String[] inputStore = new String[100];
+        int count = 0;
 
         while (exitFlag) { 
             String usr_input = scanner.nextLine();
             if (usr_input.equalsIgnoreCase("exit")) {
                 exitFlag = false;
-            } else {
-                System.out.println("\t---------------------------------\n");
-                System.out.println(usr_input);
-                System.out.println("\t---------------------------------\n");
+            }
+            else if (usr_input.equalsIgnoreCase("list")) {
+                System.out.println("\n  ---------------------------------");
+                System.out.println("  Here is your list of clownery:");
+                for (int i = 0; i < count; i++) {
+                    System.out.println("  " + (i + 1) + ". " + inputStore[i]);
+                }
+                System.out.println("  ---------------------------------\n");
+            }
+            else {
+                inputStore[count] = usr_input;
+                System.out.println("  ---------------------------------");
+                System.out.println("added: " + usr_input);
+                System.out.println("  ---------------------------------\n");
+                count++;
             }
         }
-
-        System.out.println("\tClowning complete.");
-        System.out.println("\tGoodbye fellow clown!");
-        System.out.println("\t---------------------------------\n");
+        
+        System.out.println("  ---------------------------------\n");
+        System.out.println("  Clowning complete.");
+        System.out.println("  Goodbye fellow clown!");
+        System.out.println("  ---------------------------------\n");
 
         scanner.close();
     }
